86f45313925f5ac7d0a5e54fc36b7109f717c303
Strubloid <strubloid@gmail.com>
2026-04-13 10:49:37 +0100

    starting the split screen component

--- brazucas/jest.config.ts
+++ brazucas/jest.config.ts
@@ -1,11 +1,16 @@
-import type { Config } from 'jest'
+import type { Config } from "jest";
 
 const config: Config = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-}
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
+    },
+    transformIgnorePatterns: ["/node_modules/(?!(styled-components)/)"],
+    moduleNameMapper: {
+        "^@components/(.*)$": "<rootDir>/app/components/$1",
+        "^@/(.*)$": "<rootDir>/$1",
+    },
+};
 
-export default config
+export default config;
