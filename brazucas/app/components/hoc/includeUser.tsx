f7b52ad70f2fe48ffc214c5e0b2d37ae96d55cec
Strubloid <strubloid@gmail.com>
2026-04-14 16:49:05 +0100

    adding a history home, not being used, but just to have as record of previous things, and also i added the userAddedToInfoWrapper to exemplify how to achive adding some proc like an user to an element by using the includeUser function

---
+++ brazucas/app/components/hoc/includeUser.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+/**
+ * This is a simple HOC that will include user information as a prop for a component.
+ * It can be used to provide user data to a component without passing it explicitly.
+ * @param Component The component that will receive the user information.
+ * @returns A new component that includes user information as a prop.
+ */
+export const includeUser = (Component: any, userId: number) => {
+    return (props: any) => {
+        // user to add
+        const [user, setUser] = useState(null);
+
+        // fethcing the data from the server
+        useEffect(() => {
+            (async () => {
+                const response = await axios.get(`/api/users/${userId}`);
+                setUser(response.data);
+            })();
+        }, []);
+
+        return <Component {...props} user={user} />;
+    };
+};
--- brazucas/app/page.tsx
+++ brazucas/app/page.tsx
@@ -1,15 +1,24 @@
 "use client";
+import { includeUser } from "./components/hoc/includeUser";
 import { logProps } from "./components/hoc/logProps";
 import { UserInfo } from "./components/user/UserInfo";
 
 // this will be adding the user info as a prop for log the props of the component.
 const UserInfoWrapper = logProps(UserInfo);
 
+const UserAddedToInfoWrapper = includeUser(UserInfoWrapper, 1);
+
 export default function Home() {
     return (
         <>
             <h1>Welcome to Brazucas!</h1>
-            <UserInfoWrapper test="lucas" lol="funny" age={82} />
+            <p>Soon I will develop the main app, keep waiting there...</p>
+
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+
+            <h1>HOC: Including the user</h1>
+            <UserAddedToInfoWrapper />
         </>
     );
 }
