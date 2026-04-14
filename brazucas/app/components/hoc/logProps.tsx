a57e0b422b2c505b4d338120ddd47a9de21d6f93
Strubloid <strubloid@gmail.com>
2026-04-14 16:29:09 +0100

    adding the log of the props of a component by havint the hoc logProps auround a component UserInfo

---
+++ brazucas/app/components/hoc/logProps.tsx
@@ -0,0 +1,12 @@
+/**
+ * This is a simple HOC that will log the props of a component to the console.
+ * It can be used for debugging purposes, to see what props are being passed to a component.
+ * @param Component The component whose props will be logged.
+ * @returns A new component that logs its props.
+ */
+export const logProps = (Component: any) => {
+    return (props: any) => {
+        console.log("Props:", props);
+        return <Component {...props} />;
+    };
+};
