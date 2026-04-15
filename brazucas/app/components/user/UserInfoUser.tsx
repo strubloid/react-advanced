ef68355793cfad3deb4e6ea21ced108e23ca04b6
Strubloid <strubloid@gmail.com>
2026-04-15 12:06:30 +0100

    making the user info user, as a way to save the useUser reference into the code as well as the new one resource

---
+++ brazucas/app/components/user/UserInfoUser.tsx
@@ -0,0 +1,28 @@
+import type { UserType } from "@/app/data/Users";
+import { useUser } from "../hooks/UserHook";
+
+export const UserInfoUser = ({ userId }: { userId: number }) => {
+
+    // we load the user data from the hook
+    const user = useUser(userId);
+
+    // Destructuring the variables from the user object
+    const { name, age, country, books } = user || {};
+
+    return user ? (
+        <>
+            <h1>User Info</h1>
+            <h2>{name}</h2>
+            <p>Age: {age} years</p>
+            <p>Country: {country}</p>
+            <h2>Books:</h2>
+            <ul>
+                {books?.map((book: string) => {
+                    return <li key={`${book}-${name}-book`}>{book}</li>;
+                })}
+            </ul>
+        </>
+    ) : (
+        <h1>Loading ...</h1>
+    );
+};
